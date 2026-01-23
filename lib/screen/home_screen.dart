732dbe226bf3c85b0f91bce587147a0956e06820
taixii <daniel1616@naver.com>
2026-01-23 22:30:26 +0900

    feat: request Geolocator permission

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final CameraPosition initialPosition = CameraPosition(
+    target: LatLng(37.5214, 126.9246),
+    zoom: 15,
+  );
+
+  @override
+  initState() {
+    super.initState();
+    checkPermission();
+  }
+
+  checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isLocationEnabled) {
+      throw Exception("위치 기능을 활성화 해주세요.");
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+    }
+
+    if (checkedPermission != LocationPermission.always
+    || checkedPermission != LocationPermission.whileInUse) {
+      throw Exception("위치 권한을 허가해주세요.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+                initialCameraPosition: initialPosition,
+            ),
+          ),
+        ],
       ),
     );
   }
